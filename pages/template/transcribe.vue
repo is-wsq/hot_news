47ac7b17b597533fe1383d489c3b209e170beaaf
is-wsq <103235337+is-wsq@users.noreply.github.com>
2025-04-25 11:25:56 +0800

    打包测试在线录制

--- pages/template/transcribe.vue
+++ pages/template/transcribe.vue
@@ -9,24 +9,38 @@
       <view @click='handlerOnCahnger'>
         {{!status?'开始录音':'结束录音'}}
       </view>
-      <mumu-recorder ref='recorder' @success='handlerSuccess' @error='handlerError'></mumu-recorder>
     </view>
     <button type="primary" @click='handlerSave'>保存</button>
   </view>
 </template>
 
 <script>
-  import MumuRecorder from '@/uni_modules/mumu-recorder/components/mumu-recorder/mumu-recorder.vue'
   export default {
-    components: { MumuRecorder },
     data() {
       return {
         status: false,
-        recorder: null
+        isUserMedia: false,
+        stream: null,
+        audio: null,
+        recorder: null,
+        chunks: [],
+        startTime: 0
       }
     },
+    beforeDestroy() {
+      this.stop()
+    },
     mounted() {
+      if (origin.indexOf('https') === -1) {
+        this.$emit('error', '100')
+        throw '请在 https 环境中使用本插件。'
+      }
+      if (!navigator.mediaDevices || !window.MediaRecorder) {
+        this.$emit('error', '101')
+        throw '当前浏览器不支持'
+      }
 
+      this.getRecorderManager()
     },
     methods: {
       handlerSave() {
@@ -35,16 +49,87 @@
         tag.download = '录音'
         tag.click()
       },
-      handlerOnCahnger() {
-        if (this.status) {
-          this.$refs.recorder.stop()
-        } else {
-          this.$refs.recorder.start()
-        }
+      getRecorderManager() {
+        this.audio = document.createElement('audio')
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+          this.isUserMedia = true
+          stream.getTracks().forEach((track) => {
+            track.stop()
+          })
+        }).catch(err => {
+          this.onErrorHandler(err)
+        })
+      },
+      start() {
+        if (!this.isUserMedia) return console.log('设备不支持')
+
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+          this.startTime = new Date().getTime()
+          this.stream = stream
+          this.recorder = new MediaRecorder(stream)
+          this.recorder.ondataavailable = this.getRecordingData
+          this.recorder.onstop = this.saveRecordingData
+          this.recorder.start()
+        }).catch(err => {
+          this.onErrorHandler(err)
+        })
         this.status = !this.status
       },
+      stop() {
+        this.recorder.stop()
+        this.stream.getTracks().forEach((track) => {
+          track.stop()
+        })
+        this.status = !this.status
+      },
+      getRecordingData(e) {
+        this.chunks.push(e.data)
+      },
+      saveRecordingData() {
+        const blob = new Blob(this.chunks, { 'type': 'audio/mpeg' }),
+            localUrl = URL.createObjectURL(blob)
+
+        const endTime = new Date().getTime()
+
+        let duration = (endTime - this.startTime).toString().split('')
+        duration.splice(duration.length - 2)
+        duration.splice(duration.length - 1, 0, '.')
+        duration = parseFloat(duration.join(''))
+
+        const recorder = {
+          data: blob,
+          duration: duration,
+          localUrl: localUrl
+        }
+        this.handlerSuccess(recorder)
+      },
+      onErrorHandler(err) {
+        console.log(err)
+        if (err.name === 'NotAllowedError') {
+          this.handlerError('201')
+          throw '用户拒绝了当前的浏览器实例的访问请求'
+        }
+
+        if (err.name === 'NotReadableError') {
+          this.handlerError('202')
+          throw '当前浏览器不支持'
+        }
+
+        this.handlerError('500')
+        throw '调用失败，原因不详'
+
+      },
+
+
+      handlerOnCahnger() {
+        if (this.status) {
+          this.stop()
+        } else {
+          this.start()
+        }
+      },
       handlerSuccess(res) {
-        this.$tip.confirm(JSON.stringify(res), false)
+        console.log(res)
         this.recorder = res
       },
       handlerError(code) {
