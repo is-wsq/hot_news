a8d8165c5bd66c50b0269d71ae1e20f0d1d77cf4
is-wsq <103235337+is-wsq@users.noreply.github.com>
2025-06-04 10:09:55 +0800

    修改"联系我们页面"，页面风格适配整体风格

--- pages/user/contactUs.vue
+++ pages/user/contactUs.vue
@@ -3,16 +3,16 @@
     <view class="contact-us-content">
       <uni-icons class="contact-us-back" type="left" size="21" color="#ffffff" @click="back"></uni-icons>
       <view class="contact-us-title">联系我们</view>
-      <image ref="content" src="/static/img/contact-us.jpeg" alt="背景图片" class="cover-image"/>
+      <image ref="content" src="/static/img/contact-us.png" alt="背景图片" class="cover-image"/>
     </view>
-    <view style="display: flex;align-items: center;height: 60px;border-bottom: 1px solid #ededed;border-top: 1px solid #ededed;">
-      <uni-icons type="phone" size="28" color="#409EFF" class="contact-us-icon" style="margin-left: 20px"></uni-icons>
-      <view style="flex: 1;margin-left: 20px;font-size: 16px;font-weight: bold">123-4567-890</view>
+    <view style="display: flex;align-items: center;height: 60px;border-bottom: 1px solid #444444">
+      <uni-icons type="phone" size="28" color="#ffffff" class="contact-us-icon" style="margin-left: 20px"></uni-icons>
+      <view style="flex: 1;margin-left: 20px;font-size: 16px;font-weight: bold;color: #ffffff">123-4567-890</view>
       <button class="contact-us-btn" @click="call">拨打</button>
     </view>
-    <view style="display: flex;align-items: center;height: 60px;border-bottom: 1px solid #ededed;">
-      <uni-icons type="email" size="28" color="#409EFF" class="contact-us-icon" style="margin-left: 20px"></uni-icons>
-      <view style="flex: 1;margin-left: 20px;font-size: 16px;font-weight: bold">Singai@foxmail.com</view>
+    <view style="display: flex;align-items: center;height: 60px;border-bottom: 1px solid #444444">
+      <uni-icons type="email" size="28" color="#ffffff" class="contact-us-icon" style="margin-left: 20px"></uni-icons>
+      <view style="flex: 1;margin-left: 20px;font-size: 16px;font-weight: bold;color: #ffffff">Singai@foxmail.com</view>
       <button class="contact-us-btn" @click="copyEmail">复制</button>
     </view>
     <view class="company-logo">广东省奇点未来软件研发</view>
@@ -52,7 +52,7 @@ export default {
 <style scoped>
 .contact-us {
   height: 100vh;
-  background-color: #ffffff;
+  background-color: #000000;
 }
 
 .contact-us-content {
@@ -60,7 +60,6 @@ export default {
   justify-content: center;
   line-height: 56px;
   position: relative;
-  margin-bottom: 15px;
 }
 
 .contact-us-title {
@@ -86,19 +85,20 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 10px;
+  margin-top: 20px;
   font-size: 16px;
   font-weight: bold;
+  color: #ffffff;
 }
 
 .contact-us-btn {
-  border: 1px solid #409EFF;
+  border: 1px solid #444444;
   margin-right: 20px;
   border-radius: 20px;
   height: 30px;
   line-height: 30px;
   font-size: 14px;
-  color: #409EFF;
-  background-color: #ffffff;
+  color: #ffffff;
+  background-color: #000000;
 }
 </style>
